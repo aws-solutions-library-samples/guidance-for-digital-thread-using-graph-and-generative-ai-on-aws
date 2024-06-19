122aac60aa6fd2f1c22348997146348a7de601e8
hspoon-aws <hspoon@amazon.com>
2024-06-19 11:39:00 +0800

    add new FM

--- src/genai-chatbot-app/llm_factory.py
+++ src/genai-chatbot-app/llm_factory.py
@@ -1,6 +1,7 @@
-from langchain.llms.bedrock import Bedrock
 import streamlit as st
 import boto3
+# from langchain_community.llms import Bedrock
+from langchain_aws import ChatBedrock
 
 class LLMFactory:
     def __init__(self):
@@ -10,15 +11,25 @@ class LLMFactory:
     ) 
 
     def create_llm(self, llm_model):
-        if llm_model == 'anthropic.claude-v2':
+        if llm_model == 'anthropic.claude-v2.1':
             inference_modifier = {'max_tokens_to_sample': 4096, 
                                   "temperature": 0.01,
                                   "top_k": 250,
                                   "top_p": 1,
                                   "stop_sequences": ["\n\nHuman"]}
-            llm = Bedrock(model_id="anthropic.claude-v2",
+            llm = ChatBedrock(model_id="anthropic.claude-v2.1",
                           client=self.bedrock_runtime, 
                           model_kwargs=inference_modifier)
+        elif llm_model == 'anthropic.claude-3-haiku-20240307-v1:0':
+            inference_modifier = {'max_tokens_to_sample': 4096, 
+                                  "temperature": 0.01,
+                                  "top_k": 250,
+                                  "top_p": 1,
+                                  "stop_sequences": ["\n\nHuman"]}
+            llm = ChatBedrock(model_id="anthropic.claude-3-haiku-20240307-v1:0",
+                          client=self.bedrock_runtime, 
+                          model_kwargs=inference_modifier)
+        
         else:
             st.error('LLM not supported yet')
             st.stop()
